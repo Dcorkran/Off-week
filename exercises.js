6e0096a16b47b5277aa353d671e91ff44b34ff36
Dillon Corkran <dcorkran972@gmail.com>
2016-11-22 14:16:48 -0500

    Updated REAMDME

--- exercises.js
+++ exercises.js
@@ -80,10 +80,13 @@ list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]);
 
 
 function array_diff(a, b) {
-  for (var i = 0; i < a.length; i++) {
+  // first loop works backwards through array
+  for (var i = a.length - 1; i >= 0; i--) {
+    // second loop checks each part of the first array to the second array
+    // if the current index of the first loop matches any bart of second it will be removed
     for (var j = 0; j < b.length; j++) {
       if (a[i] === b[j]) {
-        a.splice([i],[1]);
+        a.splice([i],1);
       }
     }
   }
