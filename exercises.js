2439a0c45f76bd5c53f89db7bee87cf5cc26f29b
Dillon Corkran <dcorkran972@gmail.com>
2016-11-22 14:13:03 -0500

    completed array_diff function

--- exercises.js
+++ exercises.js
@@ -77,3 +77,15 @@ function list(names){
 }
 
 list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]);
+
+
+function array_diff(a, b) {
+  for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < b.length; j++) {
+      if (a[i] === b[j]) {
+        a.splice([i],[1]);
+      }
+    }
+  }
+  return a;
+}
