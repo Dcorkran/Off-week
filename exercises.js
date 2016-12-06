69ccb3f96ac0a862710243daaef5a1c51cec24f6
Dillon Corkran <dcorkran972@gmail.com>
2016-12-05 19:02:03 -0700

    :ghost: working on string to int

--- exercises.js
+++ exercises.js
@@ -404,3 +404,48 @@ function nextBigger(n){
   }
   return -1;
 }
+
+
+
+function sumStrings(a,b) {
+  let answer = [];
+  let maxA = a.length - 1;
+  let maxB = b.length - 1;
+  const CONSTMAX = Math.max(maxA,maxB);
+  for (var i = 0; i <= CONSTMAX; i++) {
+    if (a[maxA] === undefined) {
+      if (answer[i] === undefined) {
+        answer[i] = 0;
+      }
+      answer[i] += parseInt(b[maxB]);
+      maxB--;
+    } else if (b[maxB] === undefined) {
+      if (answer[i] === undefined) {
+        answer[i] = 0;
+      }
+      answer[i] += parseInt(a[maxA]);
+      maxA--;
+    } else if (a[maxA] === undefined && b[maxB] === undefined){
+      return answer.reverse().join('');
+    } else {
+      if (answer[i] === undefined) {
+        answer[i] = 0;
+      }
+      answer[i] += parseInt(a[maxA]) + parseInt(b[maxB]) % 10;
+      if (parseInt(a[maxA]) + parseInt(b[maxB]) >= 10) {
+        answer[i + 1] = 1;
+      }
+      maxA--;
+      maxB--;
+    }
+  }
+  return answer.reverse().join('');
+}
+
+//
+// if (parseInt(a[maxA]) + parseInt(b[maxB]) >= 10) {
+//   answer[i + 1] = 1;
+//   answer[i] = parseInt(a[maxA]) + parseInt(b[maxB]) % 10;
+// } else {
+//   answer[i] = parseInt(a[maxA]) + parseInt(b[maxB]) % 10;
+// }
