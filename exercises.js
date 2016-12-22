71d0e0d7711ec5f001d9516bf9cdcdbb6b08a9a8
Dillon Corkran <dcorkran972@gmail.com>
2016-12-21 17:09:15 -0700

    :+1: added more exercises

--- exercises.js
+++ exercises.js
@@ -743,3 +743,52 @@ function iqTest(numbers){
     return oddIndex[0];
   }
 }
+
+function likes(names) {
+  if (names.length === 0) {
+    return 'no one likes this';
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+function findOutlier(integers){
+  let oddOrEven = {
+    odd:0,
+    even:0
+  };
+  for (var i = 0; i < 3; i++) {
+    if (integers[i] % 2 === 1 || integers[i] % 2 === -1) {
+      oddOrEven.odd++;
+    } else {
+      oddOrEven.even++;
+    }
+  }
+  for (var i = 0; i < integers.length; i++) {
+    if (oddOrEven.even > oddOrEven.odd) {
+      if (integers[i] % 2 === 1 ||integers[i] % 2 === -1) {
+        return integers[i];
+      }
+    } else {
+      if (integers[i] % 2 === 0 ||integers[i] % 2 === -0) {
+        return integers[i];
+      }
+    }
+  }
+}
+
+function order(words){
+  words = words.split(' ');
+  let newWords = [];
+  let regEx = /[1-9]/;
+  for (var i = 0; i < words.length; i++) {
+    newWords[parseInt(words[i].match(regEx)) - 1] = words[i];
+  }
+  return newWords.join(' ');
+}
