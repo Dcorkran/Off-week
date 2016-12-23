7151bd2b8e3a0196e84a80ecb3353dbdf52875eb
Dillon Corkran <dcorkran972@gmail.com>
2016-12-23 12:33:48 -0700

    :sparkles: new exercise complete

--- exercises.js
+++ exercises.js
@@ -792,3 +792,20 @@ function order(words){
   }
   return newWords.join(' ');
 }
+
+function findEvenIndex(arr){
+  for (let i = 1; i < arr.length; i++) {
+    let leftSide = 0;
+    for (let j = 0; j < i; j++) {
+      leftSide += arr[j];
+    }
+    let rightSide = 0;
+    for (let k = arr.length - 1; k > i; k--) {
+      rightSide += arr[k];
+    }
+    if (leftSide === rightSide) {
+      return i;
+    }
+  }
+  return -1;
+}
