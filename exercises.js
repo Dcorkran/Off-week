96f06d8a3792deb83890709c857596714804326e
Dillon Corkran <dcorkran972@gmail.com>
2016-12-24 18:39:16 -0500

    :snowman: Happy Holidays

--- exercises.js
+++ exercises.js
@@ -668,62 +668,7 @@ function spinWords(str){
 }
 
 
-function dirReduc(arr){
-  let northCount = 0, southCount = 0, eastCount = 0, westCount = 0;
-  function checkDirection(value){
-    switch (value) {
-    case 'NORTH':
-      northCount++;
-      break;
-    case 'SOUTH':
-      southCount++;
-      break;
-    case 'EAST':
-      eastCount++;
-      break;
-    case 'WEST':
-      westCount++;
-      break;
-    }
-  }
-  function deleteDirection(value, index){
-    switch (value) {
-    case 'NORTH':
-      if (southCount > 0) {
-        southCount--;
-      } else {
-        newArr.push(value);
-      }
-      break;
-    case 'SOUTH':
-      if (northCount > 0) {
-        northCount--;
-      } else {
-        newArr.push(value);
-      }
-      break;
-    case 'EAST':
-      if (westCount > 0) {
-        westCount--;
-      } else {
-        newArr.push(value);
-      }
-      break;
-    case 'WEST':
-      if (eastCount > 0) {
-        eastCount--;
-      } else {
-        newArr.push(value);
-      }
-      break;
-    }
-  };
-  arr.forEach(checkDirection);
-  let newArr = [];
-  arr.forEach(deleteDirection);
-  return newArr;
 
-}
 
 
 function iqTest(numbers){
@@ -808,4 +753,72 @@ function findEvenIndex(arr){
     }
   }
   return -1;
+}
+
+function loop_size(node){
+  loopDone = false
+  loopLength = 0;
+  while (!loopDone) {
+
+    if (true) {
+
+    }
+    node.myThing = 1;
+    node.getNext();
+  }
+}
+
+function dirReduc(arr){
+  let northCount = false, southCount = false, eastCount = false, westCount = false, newArr = [];
+  arr.forEach(deleteDirection);
+  // function checkDirection(value){
+  // switch (value) {
+  // case 'NORTH':
+  //   northCount++;
+  //   break;
+  // case 'SOUTH':
+  //   southCount++;
+  //   break;
+  // case 'EAST':
+  //   eastCount++;
+  //   break;
+  // case 'WEST':
+  //   westCount++;
+  //   break;
+  // }
+  // }
+
+  function deleteDirection(value, index, array){
+    console.log(value,index,array);
+    switch (value) {
+    case 'NORTH':
+      if (southCount > 0) {
+        southCount--;
+      } else {
+        newArr.push(value);
+      }
+      break;
+    case 'SOUTH':
+      if (northCount > 0) {
+        northCount--;
+      } else {
+        newArr.push(value);
+      }
+      break;
+    case 'EAST':
+      if (westCount > 0) {
+        westCount--;
+      } else {
+        newArr.push(value);
+      }
+      break;
+    case 'WEST':
+      if (eastCount > 0) {
+        eastCount--;
+      } else {
+        newArr.push(value);
+      }
+      break;
+    }
+  };
 }
