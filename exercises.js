691298325746b89c379e314883d32a59180025a0
Dillon Corkran <dcorkran972@gmail.com>
2017-01-10 23:14:10 -0700

    :sparkles: Jaden case exercise complete

--- exercises.js
+++ exercises.js
@@ -822,3 +822,9 @@ function dirReduc(arr){
     }
   };
 }
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map((element)=>{
+    return element[0].toUpperCase() + element.substr(1);
+  }).join(' ');
+};
