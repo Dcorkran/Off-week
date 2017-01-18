b141155480de2d573f68242966a76e7a591c2e56
Dillon Corkran <dcorkran972@gmail.com>
2017-01-17 21:35:13 -0700

    :sparkles: completed persistence exercise

--- exercises.js
+++ exercises.js
@@ -828,3 +828,18 @@ String.prototype.toJadenCase = function () {
     return element[0].toUpperCase() + element.substr(1);
   }).join(' ');
 };
+
+function persistence(num) {
+   let n = 0;
+   let multipliedDigits = 0;
+   while (num.toString().split('').length > 1) {
+     let stringNum = num.toString().split('');
+     multipliedDigits = parseInt(stringNum[0])
+     for (var i = 1; i < stringNum.length; i++) {
+       multipliedDigits *= parseInt(stringNum[i]);
+     }
+     n++;
+     num = multipliedDigits;
+   }
+  return n;
+}
