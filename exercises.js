340b2f83c5a56e32b005b8202b0cb0a17127373d
Dillon Corkran <dcorkran972@gmail.com>
2017-01-29 23:54:38 -0700

    :sparkles: RPS game

--- exercises.js
+++ exercises.js
@@ -916,3 +916,30 @@ function isValidCoordinates(coordinates){
 
   return true; // do your thing!
 }
+
+
+function rockPaperScissors(){
+  var playerChoice, computerChoice;
+  playerChoice = prompt('Choose rock, paper, or scissors');
+  computerChoice = Math.random();
+
+  if (computerChoice < .333) {
+    computerChoice = 'rock';
+  } else if (computerChoice >= .333 && computerChoice < .666) {
+    computerChoice = 'paper';
+  } else {
+    computerChoice = 'scissors';
+  }
+
+  if (playerChoice === computerChoice) {
+    alert('You chose ' + playerChoice + ', and the computer chose ' + computerChoice + '. Tie Game.')
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    alert('You chose ' + playerChoice + ', and the computer chose ' + computerChoice + '. You Win.')
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    alert('You chose ' + playerChoice + ', and the computer chose ' + computerChoice + '. You Win.')
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    alert('You chose ' + playerChoice + ', and the computer chose ' + computerChoice + '. You Win.')
+  } else {
+    alert('You chose ' + playerChoice + ', and the computer chose ' + computerChoice + '. You Lose.')
+  }
+}
