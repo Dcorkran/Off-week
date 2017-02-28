6f5c0bbf90cb3284a7242881485bfd17f693bc3b
Dillon Corkran <dcorkran972@gmail.com>
2017-02-27 21:10:57 -0700

    :+1: magic 8 ball for js-game-tutorials done

--- exercises.js
+++ exercises.js
@@ -1096,3 +1096,14 @@ function hangman(){
   alert('Congratulations, you win! The word was ' + word);
   console.log(currentAnswer);
 }
+
+function eightBall(){
+  var name = prompt('Please enter your name.');
+  var answers = ['It is certain.', 'Maybe next time.', 'Try again later.','Not a chance.','Answer unclear','My sources say no.'];
+  do {
+    console.log(prompt);
+    var question = prompt('Welcome ' + name + '. What is your question?');
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    alert('The question was: ' + question +'\n' + 'The magic 8-ball says: ' + answer);
+  } while (question !== null);
+}
