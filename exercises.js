69a45ffefbc63d84ae5b45a4308609cd42a53ce8
Dillon Corkran <dcorkran972@gmail.com>
2017-03-01 09:53:54 -0700

    :sparkles: zip function

--- exercises.js
+++ exercises.js
@@ -1111,3 +1111,18 @@ function eightBall(){
     }
   } while (question !== null);
 }
+
+
+function zip(arr1, arr2){
+  var maxArrLength = Math.max(arr1.length, arr2.length);
+  var answerArray = [];
+  for (var i = 0; i < maxArrLength; i++) {
+    if (arr1[i]) {
+      answerArray.push(arr1[i]);
+    }
+    if (arr2[i]) {
+      answerArray.push(arr2[i]);
+    }
+  }
+  return answerArray;
+}
