c74eff4f68c8d32d79a713610f81a10eeaf301a8
Dillon Corkran <dcorkran972@gmail.com>
2017-03-06 19:06:00 -0700

    :+1: more exercises

--- exercises.js
+++ exercises.js
@@ -1169,3 +1169,129 @@ function stockList(listOfArt, listOfCat){
     return answerString;
   }
 }
+
+function isPrime(num){
+  if (num === 0 || num === 1 || num === -1) {
+    return false;
+  } else if (num > 0) {
+    for (var i = num - 1; i > 1 ;i--) {
+      if ((num / i) % 1 === 0) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    for (var i = num + 1; i < -1 ;i++) {
+      if ((num / i) % 1 === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+function stairs(n){
+  for (var i = 0; i < n; i++) {
+    var row = '';
+    for (var j = 0; j < n - i; j++) {
+      row += ' ';
+    }
+    for (var k = n; k >= n - i; k--) {
+      row += '#';
+    }
+    console.log(row);
+  }
+}
+
+function threes(n){
+  while (n > 1) {
+    if ((n / 3) % 1 === 0) {
+      console.log(n, 0);
+      n /= 3;
+    } else if (((n - 1) / 3) % 1 === 0) {
+      console.log(n, -1);
+      n -= 1;
+      n /= 3;
+    } else {
+      console.log(n, 1);
+      n += 1;
+      n /= 3;
+    }
+  }
+}
+
+
+
+
+function BSTNode(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BSTNode.prototype.add = function (node) {
+  if (node.value < this.value) {
+    this.left ? this.left.add(node) : this.left = node;
+  } else {
+    this.right ? this.right.add(node) : this.right = node;
+  }
+}
+
+
+let bstn1 = new BSTNode(8);
+let bstn2 = new BSTNode(3);
+let bstn3 = new BSTNode(1);
+let bstn4 = new BSTNode(6);
+let bstn5 = new BSTNode(4);
+let bstn6 = new BSTNode(7);
+let bstn7 = new BSTNode(10);
+let bstn8 = new BSTNode(14);
+let bstn9 = new BSTNode(13);
+
+for (let i = 2; i < 10; i++) {
+  bstn1.add(window[`bstn${i}`]);
+}
+
+function search(value, node) {
+  if (value === node.value) {
+    return node;
+  } else if (value < node.value) {
+    if (node.left !== null) {
+      node = node.left;
+    } else {
+      return null;
+    }
+  } else {
+    if (node.right !== null) {
+      node = node.right;
+    } else {
+      return null;
+    }
+  }
+}
+
+function findSenior(list) {
+  let answerArr = [];
+  list.map(function(obj){
+    if (answerArr.length === 0 || answerArr[0].age < obj.age) {
+      return answerArr = [obj];
+    } else if (answerArr[0].age === obj.age) {
+      return answerArr.push(obj);
+    }
+  })
+  return answerArr;
+}
+
+function findSenior(list) {
+  let answerArr = [];
+  list.map(checkAge);
+  return answerArr;
+
+  function checkAge(obj){
+    if (answerArr.length === 0 || answerArr[0].age < obj.age) {
+      return answerArr = [obj];
+    } else if (answerArr[0].age === obj.age) {
+      return answerArr.push(obj);
+    }
+  }
+}
