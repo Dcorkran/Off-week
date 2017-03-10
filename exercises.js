8aafd005d2026dde4c80c497b2d8043202d819a2
Dillon Corkran <dcorkran972@gmail.com>
2017-03-10 15:09:43 -0700

    :sparkles: tribinachi

--- exercises.js
+++ exercises.js
@@ -1325,3 +1325,16 @@ function wordy(str){
   }
   return numArr[numArr.length - 1];
 }
+
+
+function tribonacci(signature,n){
+  var answerArr = [];
+  for (var i = 0; i < n; i++) {
+    if (i <= 2) {
+      answerArr.push(signature[i]);
+    } else {
+      answerArr.push(answerArr[i - 1] + answerArr[i - 2] + answerArr[i - 3]);
+    }
+  }
+  return answerArr;
+}
