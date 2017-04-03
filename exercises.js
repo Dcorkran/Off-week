a8de3cd7796363b86679d5085c4c38810c5fac29
Dillon Corkran <dcorkran972@gmail.com>
2017-04-03 11:30:31 -0600

    :sparkles: Replace with alphabet position

--- exercises.js
+++ exercises.js
@@ -1338,3 +1338,29 @@ function tribonacci(signature,n){
   }
   return answerArr;
 }
+
+function isMerge(s, part1, part2) {
+  if (part1.length + part2.length !== s.length) {
+    return false;
+  }
+  part1 = part1.split('');
+  part2 = part2.split('');
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === part1[0]) {
+      part1.shift();
+    } else if (s[i] === part2[0]) {
+      part2.shift();
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+function alphabetPosition(text) {
+  var regEx = /[^a-zA-Z]/g;
+  var textArr = text.toLowerCase().replace(regEx,'').split('');
+  return textArr.map((letter)=>{
+    return String(letter.charCodeAt(0) - 96);
+  }).join(' ')
+}
