0d01866d9397b123b27fd1ed154d389560d9f846
Dillon Corkran <dcorkran972@gmail.com>
2017-04-10 15:30:37 -0600

    :sparkles: compare arrays

--- exercises.js
+++ exercises.js
@@ -1496,3 +1496,23 @@ function titleCase(title, minorWords) {
     return word.substring(0,1).toUpperCase() + word.substring(1,word.length).toLowerCase();
   }
 }
+
+function comp(array1, array2){
+  if (!array2) {
+    return false
+  }
+  else if (array1.length === 0 && array2.length === 0) {
+    return true
+  } else {
+    array2 = array2.sort();
+    array1 = array1.map((e)=>{
+      return e * e;
+    }).sort();
+    for (var i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false
+      }
+    }
+    return true;
+  }
+}
