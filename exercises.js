08d9d3349ee1d074d249a488cfc514d995da35fe
Dillon Corkran <dcorkran972@gmail.com>
2017-04-12 18:54:21 -0600

    :+1: and anothaone

--- exercises.js
+++ exercises.js
@@ -1669,3 +1669,16 @@ function Mormons(startingNumber, reach, target){
     return 1 + Mormons((startingNumber * reach) + startingNumber, reach, target);
   }
 }
+
+// return the sum of all values in the tree, including the root
+function sumTheTreeValues(root){
+  if (!root.right && !root.left) {
+    return root.value;
+  } else if (root.left && root.right) {
+    return root.value + (sumTheTreeValues(root.left) + sumTheTreeValues(root.right));
+  } else if (root.left) {
+    return root.value + sumTheTreeValues(root.left)
+  } else {
+    return root.value + sumTheTreeValues(root.right)
+  }
+}
