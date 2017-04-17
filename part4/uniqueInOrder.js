93a2c2c3b354d62124cab3d94361cae83bbd2b99
Dillon Corkran <dcorkran972@gmail.com>
2017-04-17 10:51:26 -0600

    :sparkles: uniqueInOrder

---
+++ part4/uniqueInOrder.js
@@ -0,0 +1,12 @@
+var uniqueInOrder=function(iterable){
+  typeof iterable === 'string' ? iterable = iterable.split('') : iterable;
+  return iterable.filter((e,i,a)=>{
+    if (i === 0) {
+      return e
+    } else {
+      if (e !== a[i - 1]) {
+        return e;
+      }
+    }
+  })
+}
